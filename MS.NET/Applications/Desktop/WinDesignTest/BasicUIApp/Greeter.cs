64c1b58b8e367ce6f671cb619549766a7c11751a
cdacnashikfeb2019 <50868121+cdacnashikfeb2019@users.noreply.github.com>
2019-06-06 19:07:21 +0530

    Add files via upload

---
+++ MS.NET/Applications/Desktop/WinDesignTest/BasicUIApp/Greeter.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace BasicUIApp
+{
+    public partial class Greeter : Component
+    {
+        public Greeter()
+        {
+            InitializeComponent();
+        }
+
+        public Greeter(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public string Person { get; set; }
+
+        public string Period { get; set; }
+
+        [Browsable(false)]
+        public int Greetings { get; set; }
+
+        public string Greet()
+        {
+            Greetings += 1;
+            return $"Good {Period} {Person}";
+        }
+    }
+}
