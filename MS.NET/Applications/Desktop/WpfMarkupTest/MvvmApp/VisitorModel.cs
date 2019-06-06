9dacb6d99209e973ff54c50ac96e61a439ac9058
cdacnashikfeb2019 <50868121+cdacnashikfeb2019@users.noreply.github.com>
2019-06-06 19:19:48 +0530

    Add files via upload

---
+++ MS.NET/Applications/Desktop/WpfMarkupTest/MvvmApp/VisitorModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MvvmApp
+{
+    public class Visitor
+    {
+        public string Name { get; set; }
+
+        public int Frequency { get; set; } = 1;
+
+        public DateTime Recent { get; set; } = DateTime.Now;
+
+        public void Revisit()
+        {
+            Frequency += 1;
+            Recent = DateTime.Now;
+        }
+    }
+
+    public class VisitorModel
+    {
+        private Document<Visitor> dataSource;
+
+        public VisitorModel()
+        {
+            dataSource = Document.Open<Visitor>("mysite.xml");
+        }
+
+        public Visitor[] ReadVisitors()
+        {
+            return dataSource.ToArray();
+        }
+
+        public void WriteVisitor(string name)
+        {
+            Visitor visitor = dataSource.Find(e => e.Name == name);
+
+            if (visitor == null)
+                dataSource.Add(new Visitor { Name = name });
+            else
+                visitor.Revisit();
+
+            dataSource.Save();
+        }
+    }
+}
---
+++ MS.NET/Applications/Desktop/WpfMarkupTest/MvvmApp/VisitorViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmApp
+{
+    using System.Windows.Input;
+
+    public class VisitorViewModel : MvvmSupport.ViewModelBase
+    {
+        private VisitorModel model = new VisitorModel();
+
+        private string nameToRegister;
+
+        public string NameToRegister
+        {
+            get => nameToRegister;
+            set => SetProperty(ref nameToRegister, value);
+        }
+
+        public IEnumerable<Visitor> Visitors => model.ReadVisitors();
+
+        private bool CanExecuteRegister(object parameter)
+        {
+            return nameToRegister?.Length >= 3;
+        }
+
+        private void ExecuteRegister(object parameter)
+        {
+            model.WriteVisitor(nameToRegister);
+            OnPropertyChanged(() => Visitors);
+        }
+
+        public ICommand Register => DispatchCommand();
+    }
+}
